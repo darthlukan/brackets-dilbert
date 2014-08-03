d0ae2af1743355b458cd5d818c19ba997894d6e8
Brian Tomlinson <brian@smartm.no>
2014-08-03 17:51:30 +0200

    Grab today's comic page.

--- main.js
+++ main.js
@@ -1,26 +1,55 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global define, $, brackets, window */
 
-/** Simple extension that adds a "File > Hello World" menu item */
 define(function (require, exports, module) {
     "use strict";
 
     var CommandManager = brackets.getModule("command/CommandManager"),
-        Menus          = brackets.getModule("command/Menus");
+        Menus          = brackets.getModule("command/Menus"),
+        Dialogs        = brackets.getModule("widgets/Dialogs");
 
 
     // Function to run when the menu item is clicked
-    function getComic() {
-        window.alert("Hello, world!");
+    function dilbertHandler() {
+        var baseUrl = "http://www.dilbert.com/";
+        var comicBaseUrl = baseUrl + "strips/comic/";
+        var today = grabDate();
+        var requestUrl = comicBaseUrl + today + "/";
+        var html = "";
+        $.ajax({
+            url: requestUrl,
+            async: false,
+            type: 'GET',
+            success: function(data) {
+                html = data;
+                return html;
+            }
+        });
+        console.log("page: ");
+        console.log(html); // TODO: Here's our HTML, now parse it.
     }
 
+    function grabDate() {
+        var today = new Date();
+        var day = today.getDate();
+        var month = today.getMonth()+1;
+        var year = today.getFullYear();
 
-    // First, register a command - a UI-less object associating an id to a handler
-    var MY_COMMAND_ID = "brackets-dilbert.getComic";   // package-style naming to avoid collisions
-    CommandManager.register("Daily Dilbert", MY_COMMAND_ID, getComic);
+        if (day < 10) {
+            day = '0' + day;
+        }
 
-    // Then create a menu item bound to the command
-    // The label of the menu item is the name we gave the command (see above)
+        if (month < 10) {
+            month = '0' + month;
+        }
+
+        today = year + "-" + month + "-" + day;
+        return today;
+    }
+
+    var MY_COMMAND_ID = "brackets-dilbert.getComic";
+    CommandManager.register("Daily Dilbert", MY_COMMAND_ID, dilbertHandler);
+
     var menu = Menus.getMenu(Menus.AppMenuBar.HELP_MENU);
     menu.addMenuDivider();
     menu.addMenuItem(MY_COMMAND_ID);
